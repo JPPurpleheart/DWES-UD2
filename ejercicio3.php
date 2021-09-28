7732e21c2d169f84f289e69111acc803732775cd
José Pablo Machuca González <71435599+JPPurpleheart@users.noreply.github.com>
2021-09-28 13:13:24 +0200

    ejercicio3.php

---
+++ ejercicio3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $preciop = 29.99;
+        $cantp = 3;
+        $precioc = 25.99;
+        $cantc = 2;
+        $desc = 0.08;
+        $total = ($preciop * $cantp) + ($precioc * $cantc);
+        $totald = $total * $desc;
+        echo "El precio de un pantalón es de $preciop";
+        echo "El precio de una camisa es de $precioc";
+        printf("El total sin descuento es de %0.2f", $total);
+        printf("El total con descuento es de %0.2f", $totald);
+    ?>
+</body>
+</html>
