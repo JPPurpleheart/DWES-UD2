b12437be310f2a297cc65612c1bac57b620851c6
José Pablo Machuca González <71435599+JPPurpleheart@users.noreply.github.com>
2021-09-29 10:56:57 +0200

    ejercicio8.php

---
+++ ejercicio8.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $var1 = "Hola Mundo";
+        echo $var1;
+        echo is_float($var1);
+        echo is_bool($var1);
+        echo is_integer($var1);
+        $var1 = 5.72;
+        echo is_float($var1);
+        echo is_bool($var1);
+        echo is_integer($var1);
+        echo settype($var1, "integer");
+        $var2 = "6tres";
+        echo $var2;
+        echo settype($var2, "integer");
+    ?>
+</body>
+</html>
